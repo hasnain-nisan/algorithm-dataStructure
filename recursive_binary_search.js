b4918b788fbc2bbfca696c6fd9ea470f331f6b8a
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-09-07 16:12:49 +0600

    recursive binary search

---
+++ recursive_binary_search.js
@@ -0,0 +1,33 @@
+const recursiveBinarySearch = (array, result) => {
+    if(array.length <= 0){
+        return false;
+    } else {
+        let midpoint = Math.floor(array.length/2)
+        if(array[midpoint] === result){
+            return true;
+        } else {
+            if(array[midpoint] < result){
+                return recursiveBinarySearch(array.slice(midpoint + 1), result)
+            } else {
+                return recursiveBinarySearch(array.slice(0, midpoint), result);
+            }
+        }
+    }
+}
+
+const verifyBinarySearch = (index) => {
+  index === false
+    ? console.log("Result not found in the array")
+    : console.log(`Result found in the array`);
+};
+
+const array = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+];
+
+const result = recursiveBinarySearch(array, 15);
+const result1 = recursiveBinarySearch(array, 600);
+
+verifyBinarySearch(result);
+verifyBinarySearch(result1);
