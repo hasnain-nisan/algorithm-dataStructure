5954db7d558e7652232a6d3e5dd34b9558a32d1c
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-09-07 16:15:21 +0600

    recursive binary search

--- linear_search.js
+++ linear_search.js
@@ -25,7 +25,9 @@ const betterLinearSearch = (array, result) => {
 }
 
 const verifyLinearSearch = (index) => {
-    index === null ? console.log("Result not found in the array") : console.log(`Result found in the index number: ${index}`)
+    index === null 
+        ? console.log("Result not found in the array") 
+        : console.log(`Result found in the index number: ${index}`)
 }
 
 const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,];
